1e1b6352beed839c327a2299ae68772aa74f4419
IluaPlank <gerevan1994@gmail.com>
2021-11-03 12:57:23 +0500

    first commit

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    //складываются 2 числа
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    //минусуются 2 числа
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    //умножаются 2 числа
+    BinaryOperator<Integer> devide = (x, y) -> y > 0 ? x / y : 0 ;
+    //исправил devide , что б при y < 0 , не появлялась ошибка, теперь всегда будет писать 0
+    //BinaryOperator<Integer> devide = (x, y) -> x / y;
+    // делятся 2 числа(выражение до исправления)
+    UnaryOperator<Integer> pow = x -> x * x;
+    // возводится число в квадрат
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    //х больше нуля,то равно х , меньше равно х*-1
+
+    Predicate<Integer> isPositive = x -> x > 0;
+    //больше 0 , тру
+
+    Consumer<Integer> println = System.out::println;
+}
